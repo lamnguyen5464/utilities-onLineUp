74b5f2a82d7ec7dbf5968c49f59c381e453212cc
lamnguyen5464 <lamnguyen5464@gmail.com>
2021-11-08 23:12:50 +0700

    deploy + config link

--- src/App.js
+++ src/App.js
@@ -5,19 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a href="com.onlineup://somthing/anotherthing?paramar=1">
+          Open the app!
         </a>
-        <a href="myapp://somthing/anotherthing">Open the app!</a>
+        <a href="https://onlineup-2cd8d.web.app?link=1">test!</a>
       </header>
     </div>
   );
